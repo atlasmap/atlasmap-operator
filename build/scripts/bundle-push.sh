c7a0e44ec055eec7a9f6ab67b8cbb7b89fa64511
James Netherton <jamesnetherton@gmail.com>
2020-03-16 09:34:29 +0000

    Validate OLM bundle before push

--- build/scripts/bundle-push.sh
+++ build/scripts/bundle-push.sh
@@ -18,29 +18,33 @@ cp ${MANIFEST_DIR}/atlasmap-operator.package.yaml ${BUNDLE_DIR}
 cp ${MANIFEST_DIR}/${BUNDLE_VERSION}/*.yaml ${BUNDLE_DIR}
 
 if [[ -z "${QUAY_API_TOKEN}" ]]; then
-    if [[ -z "$QUAY_USERNAME" ]]; then
-        echo -n "Quay Username: "
-        read QUAY_USERNAME
-    fi
+  if [[ -z "$QUAY_USERNAME" ]]; then
+      echo -n "Quay Username: "
+      read QUAY_USERNAME
+  fi
 
-    if [[ -z "$QUAY_PASSWORD" ]]; then
-        echo -n "Quay Password: "
-        read -s QUAY_PASSWORD
-        echo
-    fi
+  if [[ -z "$QUAY_PASSWORD" ]]; then
+      echo -n "Quay Password: "
+      read -s QUAY_PASSWORD
+      echo
+  fi
 
-    QUAY_API_TOKEN=$(curl -sH "Content-Type: application/json" -XPOST https://quay.io/cnr/api/v1/users/login -d '
-    {
-        "user": {
-            "username": "'"${QUAY_USERNAME}"'",
-            "password": "'"${QUAY_PASSWORD}"'"
-        }
-    }' | jq -r '.token')
+  QUAY_API_TOKEN=$(curl -sH "Content-Type: application/json" -XPOST https://quay.io/cnr/api/v1/users/login -d '
+  {
+      "user": {
+          "username": "'"${QUAY_USERNAME}"'",
+          "password": "'"${QUAY_PASSWORD}"'"
+      }
+  }' | jq -r '.token')
 fi
 
 if which operator-courier > /dev/null; then
-    operator-courier --verbose push ${BUNDLE_DIR} ${NAMESPACE} ${REPOSITORY} ${BUNDLE_VERSION} "${QUAY_API_TOKEN}"
+  operator-courier --verbose verify --ui_validate_io ${BUNDLE_DIR}
+  operator-courier --verbose push ${BUNDLE_DIR} ${NAMESPACE} ${REPOSITORY} ${BUNDLE_VERSION} "${QUAY_API_TOKEN}"
 else
-    docker run -ti --rm -v ${BUNDLE_DIR}:${BUNDLE_DIR} jamesnetherton/operator-courier:latest \
-        operator-courier --verbose push ${BUNDLE_DIR} ${NAMESPACE} ${REPOSITORY} ${BUNDLE_VERSION} "${QUAY_API_TOKEN}"
+  docker run -ti --rm -v ${BUNDLE_DIR}:${BUNDLE_DIR} jamesnetherton/operator-courier:latest \
+    operator-courier --verbose verify --ui_validate_io ${BUNDLE_DIR}
+
+  docker run -ti --rm -v ${BUNDLE_DIR}:${BUNDLE_DIR} jamesnetherton/operator-courier:latest \
+    operator-courier --verbose push ${BUNDLE_DIR} ${NAMESPACE} ${REPOSITORY} ${BUNDLE_VERSION} "${QUAY_API_TOKEN}"
 fi
